d5969d392fd864cc4f99d6747697bdaaf23e350b
Andreas Ek <andreas@andreasek.se>
2015-08-26 18:05:13 +0200

    pull updated

--- scripts/deployer/pull.php
+++ scripts/deployer/pull.php
@@ -2,29 +2,29 @@
 
 task( 'pull:dump', function () {
 
-    writeln( 'Creating a new database dump (approx. 60s)' );
-    run( 'mysqldump -u skolporten -pArkitekt1 skolporten > ~/backup/skolporten.sql' );
+    writeln( 'Creating a new database dump (approx. 10s)' );
+    run( 'cd current/web && wp db export' );
 
 } );
 
 task( 'pull:fetch_dump', function () {
 
-    writeln( 'Getting database dump (approx. 60s)' );
-    runLocally( 'scp skolporten@www.skolporten.se:/home/httpd/skolporten/backup/skolporten.sql skolporten.sql', 999 );
+    writeln( 'Getting database dump (approx. 10s)' );
+    runLocally( 'scp root@www.andreasek.se:/mnt/persist/www/andreasek.se/current/web/andreasek.sql andreasek.sql', 999 );
 
 } );
 
 task( 'pull:resolve_dump', function () {
 
-    writeln( 'Restore database inside vagrant (approx. 60s)' );
-    runLocally( 'cd ../vagrant && vagrant ssh -c "mysql -u root -proot skolporten < /var/www/skolporten/skolporten.sql" && cd ../skolporten.dev', 999 );
+    writeln( 'Restore database inside vagrant (approx. 10s)' );
+    runLocally( 'cd ../vagrant && vagrant ssh -c "mysql -u root -proot aek < /var/www/aek.dev/andreasek.sql" && cd ../aek.dev', 999 );
 
 } );
 
 task( 'pull:set_vagrant_wp', function () {
 
     writeln( 'Restore wp after pull (approx. 60s)' );
-    runLocally( 'cd ../vagrant && vagrant ssh -c "cd /var/www/skolporten/web && wp search-replace www.skolporten.se skolporten.dev && wp plugin deactivate w3-total-cache && wp plugin activate query-monitor && wp rewrite flush" && cd ../skolporten.dev',
+    runLocally( 'cd ../vagrant && vagrant ssh -c "cd /var/www/aek.dev/web && wp search-replace www.andreasek.se aek.dev && cd ../aek.dev',
         999 );
 
 } );
@@ -32,7 +32,7 @@ task( 'pull:set_vagrant_wp', function () {
 task( 'pull:uploads', function () {
 
     writeln( 'Getting uploads, long duration first time! (approx. 60-999s)' );
-    runLocally( 'rsync -re ssh skolporten@www.skolporten.se:/home/httpd/skolporten/skolporten.se/shared/web/app/uploads web/app',
+    runLocally( 'rsync -re ssh root@www.andreasek.se:/mnt/persist/www/andreasek.se/shared/web/app/uploads web/app',
         999 );
 
 } );
